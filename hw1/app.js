323ef9aa0dfa154a206a501c3ca02223c62a9699
嘉佳 沈 <jshen97@bu.edu>
2018-06-07 15:14:31 -0400

    hw2

--- hw1/app.js
+++ hw1/app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+//const usersRouter = require('./routes/users');
 
-var app = express();
-var hw1 = require('./routes/hw1');
+const app = express();
+const hw1 = require('./routes/hw1');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
